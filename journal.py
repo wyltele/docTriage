69b1f0f6648888639792fe6f34517586b4e0ec6a
wyltele <wyltele@hotmail.com>
2019-11-03 15:45:00 -0500

    change mp meaning

--- journal.py
+++ journal.py
@@ -7,11 +7,11 @@ from openpyxl import load_workbook
 
 class journal:
 	def run(self):
-		filelist=glob.glob('C:\\Users\\hengyi\\Google Drive\\[ch]_*')
+		filelist=glob.glob('C:\\Users\\hengyi\\Google Drive\\[cho]_*')
 #		f=codecs.open('C:\\Users\\hengyi\\Google Drive\\Jingjing\\journal.csv','a',encoding="utf-8")
 		wb=load_workbook('C:\\Users\\hengyi\\Google Drive\\python\\journal.xlsx')
 		ws = wb.worksheets[0]
-		
+
 		if len(filelist)==0:
 			print('nothing to process, exit.\n')
 			sys.exit()
@@ -23,7 +23,7 @@ class journal:
 #				f.write(ent.date+','+ent.amount+','+ent.merchant+','+ent.misc+'\n')
 #		f.close()
 		wb.save('C:\\Users\\hengyi\\Google Drive\\python\\journal.xlsx')
-		
+
 class receipt_entry:
 	def __init__(self, filename):
 		self.fname=filename
@@ -65,17 +65,17 @@ class receipt_entry:
 				elif self.misc=='dz':
 					self.misc='电子产品'
 				elif self.misc=='mp':
-					self.misc='门票'
+					self.misc='娱乐'
 				else:
 					pass
-			
+
 				target=self.localdrv+self.misc+'\\'
 				if not os.path.exists(target):
 					os.makedirs(target)
 				shutil.move(self.fname,target)
-			
+
 def main():
 	journal().run()
-  
+
 if __name__== "__main__":
 	main()
