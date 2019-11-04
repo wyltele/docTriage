5ea47c0bdd1889f30f4e40896f252645acc73dd3
wyltele <wyltele@hotmail.com>
2019-11-03 19:06:46 -0500

    enable config file

--- journal.py
+++ journal.py
@@ -3,33 +3,33 @@ import shutil
 import glob
 import sys
 import codecs
+import json
 from openpyxl import load_workbook
 
 class journal:
-	def run(self):
-		filelist=glob.glob('C:\\Users\\hengyi\\Google Drive\\[cho]_*')
-#		f=codecs.open('C:\\Users\\hengyi\\Google Drive\\Jingjing\\journal.csv','a',encoding="utf-8")
-		wb=load_workbook('C:\\Users\\hengyi\\Google Drive\\python\\journal.xlsx')
-		ws = wb.worksheets[0]
+	def __init__(self):
+		cfgfile='C:\\Users\\hengyi\\Google Drive\\python\\journal.json'
+		with codecs.open(cfgfile,'r','utf-8') as f:
+			self.cfg=json.load(f)
+		heads=''.join([i for i in self.cfg['triage']['base']])
+		self.filelist=glob.glob(self.cfg["triage"]["search-location"]+'['+heads+']_*')
+		self.wb=load_workbook(self.cfg['triage']['excel'])
 
-		if len(filelist)==0:
+	def run(self):
+		if len(self.filelist)==0:
 			print('nothing to process, exit.\n')
 			sys.exit()
-		for file in filelist:
-			ent=receipt_entry(file)
-			ent.move()
-			if ent.valid:
-				ws.append([ent.date,ent.amount,ent.merchant,ent.misc])
-#				f.write(ent.date+','+ent.amount+','+ent.merchant+','+ent.misc+'\n')
-#		f.close()
-		wb.save('C:\\Users\\hengyi\\Google Drive\\python\\journal.xlsx')
+		for filename in self.filelist:
+			ent=receipt_entry(filename,self.wb,self.cfg)
+			ent.move_and_log()
+		self.wb.save(self.cfg['triage']['excel'])
 
 class receipt_entry:
-	def __init__(self, filename):
+	def __init__(self, filename,wb,cfg):
+		self.wb=wb
+		self.cfg=cfg
 		self.fname=filename
 		fields=os.path.splitext(os.path.basename(filename))[0].split("_")
-		self.googledrv='C:\\Users\\hengyi\\Google Drive\\J BRANDS JOUNALS\\Journals 2018\\'
-		self.localdrv='C:\\Users\\hengyi\\Downloads\\receipts\\'
 		if len(fields)!=5:
 			self.valid=False
 		else:
@@ -39,40 +39,17 @@ class receipt_entry:
 			self.misc=fields[2]
 			self.amount=fields[3]
 			self.merchant=fields[4]
-	def move(self):
+	def move_and_log(self):
 		if self.valid:
-			if self.type=='c':
-				shutil.move(self.fname,self.googledrv)
+			if self.misc in self.cfg["triage"]["categories"]:
+				to_dir=self.cfg["triage"]["base"][self.type]+self.cfg["triage"]["categories"][self.misc]
 			else:
-				if self.misc=='mc':
-					self.misc='买菜'
-				elif self.misc=='yf':
-					self.misc='衣服'
-				elif self.misc=='qy':
-					self.misc='汽油或车'
-				elif self.misc=='jy':
-					self.misc='教育'
-				elif self.misc=='ry':
-					self.misc='日用品'
-				elif self.misc=='fd':
-					self.misc='饭店'
-				elif self.misc=='ly':
-					self.misc='旅游'
-				elif self.misc=='jj':
-					self.misc='家居'
-				elif self.misc=='yl':
-					self.misc='医疗'
-				elif self.misc=='dz':
-					self.misc='电子产品'
-				elif self.misc=='mp':
-					self.misc='娱乐'
-				else:
-					pass
-
-				target=self.localdrv+self.misc+'\\'
-				if not os.path.exists(target):
-					os.makedirs(target)
-				shutil.move(self.fname,target)
+				to_dir=self.cfg["triage"]["base"][self.type]+self.misc
+			if not os.path.exists(to_dir):
+				os.makedirs(to_dir)
+			shutil.move(self.fname,to_dir)
+			ws=self.wb[self.cfg["triage"]["sheets"][self.type]]
+			ws.append([self.date,self.amount,self.merchant,self.misc])
 
 def main():
 	journal().run()
